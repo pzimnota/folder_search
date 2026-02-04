68524dd8fb27cd5d6b82f905fd9f27625456aee5
Paweł Zimnota <pawel.zimnota@protonmail.com>
2026-02-04 22:55:30 +0100

    Added FileCabinet logic

--- src/main/java/org/example/FileCabinet.java
+++ src/main/java/org/example/FileCabinet.java
@@ -1,24 +1,49 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class FileCabinet implements Cabinet{
-    private List<Folder> folders;
+public class FileCabinet implements Cabinet {
+    private final List<Folder> folders;
+
+    public FileCabinet(List<Folder> folders) {
+        this.folders = folders != null ? folders : new ArrayList<>();
+    }
 
     @Override
     public Optional<Folder> findFolderByName(String name) {
-        return Optional.empty();
+        FindFolderByNameVisitor visitor = new FindFolderByNameVisitor(name);
+        traverse(folders, visitor);
+        return visitor.getFoundFolder();
     }
 
     @Override
     public List<Folder> findFolderBySize(String size) {
-        return List.of();
+        FindFoldersBySizeVisitor visitor = new FindFoldersBySizeVisitor(size);
+        traverse(folders, visitor);
+        return visitor.getFolderList();
     }
 
     @Override
     public int count() {
-        return 0;
+        CountVisitor visitor = new CountVisitor();
+        traverse(folders, visitor);
+        return visitor.getFolderCount();
+    }
+
+    private boolean traverse(List<Folder> folders, FolderVisitor visitor) {
+
+        for (Folder folder : folders) {
+            boolean shouldContinue = visitor.visit(folder);
+            if (!shouldContinue) return false;
+            if (folder instanceof MultiFolder multiFolder) {
+                boolean continueRecursion = traverse(multiFolder.getFolders(), visitor);
+                if (!continueRecursion) return false;
+            }
+        }
+
+        return true;
     }
 }
 
